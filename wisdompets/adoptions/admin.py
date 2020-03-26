6d95243cd712bf2128ba309d5f3a9983da41e9fd
Merolin Christie <8525438+merolinpeter@users.noreply.github.com>
2020-03-26 13:54:27 -0700

    [Merolin] registering the models in admin (#1)
    Co-authored-by: Christie Merolin <merolinpeter@gmail.com>

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pet
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age', 'breed', 'species', 'sex']
